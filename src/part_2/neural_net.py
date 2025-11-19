9cf89e2b917fb5f83ee07f8da2432b3bc80c8e37
Miguel Angel Cabanillas <miguelangelcabanillas@MacBook-Air-de-Miguel.local>
2025-11-19 23:23:42 +0100

    Add required variables

--- src/part_2/neural_net.py
+++ src/part_2/neural_net.py
@@ -4,7 +4,7 @@ class NeuralNet:
     def __init__(self, layers):
         self.L = len(layers)
         self.n = layers.copy()
-
+        
         self.xi = [np.zeros(l) for l in layers]
 
         self.w = []
@@ -12,10 +12,23 @@ class NeuralNet:
         for lay in range(1, self.L):
             self.w.append(np.zeros((layers[lay], layers[lay - 1])))
 
+        self.theta = [np.zeros(l) for l in layers]
+
+        self.delta = [np.zeros(l) for l in layers]
+
+        self.d_w = [np.zeros((1, 1))]
+        self.d_theta = [np.zeros(l) for l in layers]
+
+        self.d_w_prev = [np.zeros((1, 1))]
+        self.d_theta_prev = [np.zeros(l) for l in layers]
+
 layers = [4, 9, 5, 1]
 nn = NeuralNet(layers)
 
 print("L =", nn.L)
 print("n =", nn.n)
-print("xi =", nn.xi)
-print("w[1] =", nn.w[1])
+print("theta[1] =", nn.theta[1])
+print("delta[1] =", nn.delta[1])
+print("d_w[1] =", nn.d_w)
+print("d_theta_prev[1] =", nn.d_theta_prev[1])
+
